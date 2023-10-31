a4ef7ee115a55cbc8e8084740d3f2688a8dc2ca3
SMCastr <mariesweetycastro@gmail.com>
2023-10-31 14:06:17 -0400

    fixing code-module

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3.
 """ Amenity Module for HBNB project"""
-
-
+from models.base_model import BaseModel
+    
+class datetime:
+    """ datetime Module for HBNB project"""
+from datetime import datetime
 from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """Amenity class that inherits from BaseModel."""
     name: str = ""
 
-class datetime
-""" datetime Module for HBNB project"""
-
-
-def __init__(self, *args, **kwargs):
-    """Instantiates a new Amenity."""
-    super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new Amenity."""
+        super().__init__(*args, **kwargs)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
--- models/review.py
+++ models/review.py
@@ -10,19 +10,53 @@ class Review(BaseModel):
     place_id: str = ""
     user_id: str = ""
     text_id: str = ""
-    
-    def place_id(self):
-        """Returns the place id."""
-        
-        return self.place_id
-    def user_id(self):
-        """Returns the user id."""
-        
-        return self.user_id
+
+def __init__(self, *args, **kwargs):
+    class Review(BaseModel):
+        """Review class that inherits from BaseModel."""
+
+        place_id: str = ""
+        user_id: str = ""
+        text_id: str = ""
+
+        def __init__(self, *args, **kwargs):
+            """Instantiates a new Review."""
+            super().__init__(*args, **kwargs)
+
+        def place_id(self):
+            """Returns the place id."""
+            return self.place_id
+
+        def user_id(self):
+            """Returns the user id."""
+            return self.user_id
+
+        def text_id(self):
+            """Returns the text id."""
+            if self.text is not None:
+                return self.text
+            return self.text_id
+        """Review class that inherits from BaseModel."""
+
+        place_id: str = ""
+        user_id: str = ""
+        text_id: str = ""
+
+        def __init__(self, *args, **kwargs):
+            """Instantiates a new Review."""
+            super().__init__(*args, **kwargs)
+
+        def place_id(self):
+            """Returns the place id."""
+            return self.place_id
+
+        def user_id(self):
+            """Returns the user id."""
+            return self.user_id
+
     def text_id(self):
         """Returns the text id."""
-        
+  
         if self.text is not None:
-        
-        
+            return self.text
         return self.text_id
