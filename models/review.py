a004f15cdd015684d0c90e2f0acc7d3620da9482
SMCastr <mariesweetycastro@gmail.com>
2023-10-31 13:41:17 -0400

    add code functions

--- models/review.py
+++ models/review.py
@@ -6,7 +6,23 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """Review class that inherits from BaseModel."""
-    place_id = ""
-    user_id = ""
-    text = ""
     
+    place_id: str = ""
+    user_id: str = ""
+    text_id: str = ""
+    
+    def place_id(self):
+        """Returns the place id."""
+        
+        return self.place_id
+    def user_id(self):
+        """Returns the user id."""
+        
+        return self.user_id
+    def text_id(self):
+        """Returns the text id."""
+        
+        if self.text is not None:
+        
+        
+        return self.text_id
