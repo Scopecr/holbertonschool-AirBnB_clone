ce90c7f9c7f4e45efc08376d0478c786d3622030
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-31 14:22:10 -0400

    Amenity code updated and corrected

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,8 @@
-#!/usr/bin/python3.
-""" Amenity Module for HBNB project"""
-
-
+#!/usr/bin/python3
+"""Defines the Amenity class"""
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
-    """Amenity class that inherits from BaseModel."""
-    name: str = ""
-
-class datetime
-""" datetime Module for HBNB project"""
-
-
-def __init__(self, *args, **kwargs):
-    """Instantiates a new Amenity."""
-    super().__init__(*args, **kwargs)
+    """Represent an Amenity object"""
+    name = ""
