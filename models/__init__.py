d3b25b4a1d5b6fc9b28ec7030f706cb200fd2b30
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-11-02 16:53:54 -0400

    fixed bug with init

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 """__init__ magic method for models dictionary"""
 
 from models.engine.file_storage import FileStorage
-
+# FileStorage instance
 storage = FileStorage()
+# & Reload objects
 storage.reload()
